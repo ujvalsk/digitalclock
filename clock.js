f2dbe60ba84cd6bc6de3bf52a38d5f17fd572bf7
UJVAL KRISHNEGOWDA <ujvalsk@gmail.com>
2023-09-19 12:16:24 +0530

    final

--- clock.js
+++ clock.js
@@ -1,14 +1,23 @@
-const WEEK =["SUN","MON","TUE","WED","THU","FRI","SAT"];
-function update time(){
+const WEEK = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function updateTime() {
     var now = new Date();
-    document.getElementById("time").innerText= 
-    zeropadding(now.getHours(),2)+":"+zeropadding(now.getMinutes(),2)+":"+zeropadding(now.getSeconds(),2);
-    document.getElementById("date").innerText= 
-    now.getFullYear()+"-"+
-    zeropadding(now.getMonth()+1,2)+"-"+zeropadding(now.getDate(),2)+" "+WEEK(now.getDay());
+
+    document.getElementById("time").innerText =
+            zeroPadding(now.getHours(), 2) + ":" +
+            zeroPadding(now.getMinutes(), 2) + ":" +
+            zeroPadding(now.getSeconds(), 2);
+
+    document.getElementById("date").innerText =
+            now.getFullYear() + "-" +
+            zeroPadding(now.getMonth() + 1, 2) + "-" +
+            zeroPadding(now.getDate(), 2) + " " +
+            WEEK[now.getDay()];
 }
+
 updateTime();
-setInterval(updateTime,1000);
-function zeropadding(num,digit){
-    return String(num).padStart(digit,'0');
+setInterval(updateTime, 1000);
+
+function zeroPadding(num, digit) {
+    return String(num).padStart(digit, '0');
 }
